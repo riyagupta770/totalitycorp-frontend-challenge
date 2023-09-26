f17ad1e01f72224233e8c54fc014719b64bd79d1
Riya Gupta <65082616+riyagupta770@users.noreply.github.com>
2023-09-26 08:45:07 +0530

    Cart.js

---
+++ Cart.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  // Implement cart management functions (add, remove, update quantity)
+
+  return (
+    <div>
+      {/* Render cart items */}
+    </div>
+  );
+};
+
+export default Cart;
