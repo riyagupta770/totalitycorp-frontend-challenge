e00624b37372162b59b5c25bc228eeb8901fc921
Riya Gupta <65082616+riyagupta770@users.noreply.github.com>
2023-09-26 08:45:34 +0530

    Checkout.js

---
+++ Checkout.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Checkout = () => {
+  const [shippingInfo, setShippingInfo] = useState({});
+  const [paymentInfo, setPaymentInfo] = useState({});
+
+  // Implement checkout form and logic
+
+  return (
+    <div>
+      {/* Render checkout form */}
+    </div>
+  );
+};
+
+export default Checkout;
